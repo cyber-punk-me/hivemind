50fb8e9510dddb116a445b8da2cd56715d47a25d
Kirill Yashchuk <arpeggiator@yandex.ru>
2018-07-19 23:17:57 +0300

    tf docker howtos

--- restart_tensorflow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# http://localhost:8888/?token=8d7bbb762478fa8de5418d45e3665b39c95449cd9468acfc
-# restart
-docker restart tensorflow
-# ssh
-docker exec -i -t tensorflow bash
-docker logs tensorflow
-docker exec -it tensorflow jupyter notebook list
--- run_tensorflow.sh
+++ run_tensorflow.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-docker run --name tensorflow -p 8888:8888 -v /opt/tf_data:/data tensorflow/tensorflow
+#https://www.tensorflow.org/serving/docker
+#https://github.com/tensorflow/serving/blob/master/tensorflow_serving/g3doc/serving_advanced.md
 
+mkdir -p /tmp/tfserving
+cd /tmp/tfserving
+git clone --depth=1 https://github.com/tensorflow/serving
+
+docker run -d --name tf_docker -p 8501:8501 -v /tmp/tfserving/serving/tensorflow_serving/servables/tensorflow/testdata/saved_model_half_plus_three:/models/half_plus_three -e MODEL_NAME=half_plus_three -t tensorflow/serving tf_docker
+
+#train a model(TODO)
+docker exec -i -t tf_docker bazel-bin/tensorflow_serving/example/mnist_saved_model /tmp/mnist_model
+docker exec -i -t tf_docker bazel build -c opt //tensorflow_serving/example:mnist_saved_model
+
+
+#feed data(prebuilt model)
+curl -d '{"instances": [1.0, 2.0, 5.0]}' -X POST http://localhost:8501/v1/models/half_plus_three:predict
+
+#feed from docker(prebuilt model)
+docker exec -i -t tf_docker curl -d '{"instances": [1.0, 2.0, 5.0]}' -X POST http://localhost:8501/v1/models/half_plus_three:predict
+
+
+docker stop tf_docker
+docker rm -f tf_docker
