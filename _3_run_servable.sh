f4e1e5a96c4948239066f0f05168516c350dc259
kyr7 <kirillskiy@gmail.com>
2018-09-01 00:13:47 +0300

    ml file structure

--- _3_run_servable.sh
+++ _3_run_servable.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker run -p 8501:8500 \
---mount type=bind,source=$(pwd)/local/model/myo-1/data,target=/models/myo-1 \
--e MODEL_NAME=myo-1 -t tensorflow/serving &
+--mount type=bind,source=$(pwd)/local/model/model-1/data,target=/models/model-1 \
+-e MODEL_NAME=model-1 -t --name model-1-servable tensorflow/serving &
---
+++ _88_rebuild_model.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./_99_cleanup.sh
+./_1_build_script.sh
+./_2_train_model.sh
