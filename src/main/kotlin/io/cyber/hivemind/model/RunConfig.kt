0f346355851528dd2690da778c4775eb127a373d
kyr7 <kirillskiy@gmail.com>
2018-11-06 12:41:06 +0300

    fix runtime

--- src/main/kotlin/io/cyber/hivemind/model/RunConfig.kt
+++ src/main/kotlin/io/cyber/hivemind/model/RunConfig.kt
@@ -4,13 +4,13 @@ data class RunConfig(val cmd : List<String>, val image: String, val opt : Map<St
 
     fun isExportSession() = getBoolOption(EXPORT_SESSION)
     fun isPullImages() = getBoolOption(PULL_IMAGES)
-    fun getRuntime() = opt?.get(IMAGE)
+    fun getRuntime() = opt?.get(RUNTIME)
 
     private fun getBoolOption(key : String) : Boolean = opt?.getOrDefault(key, FALSE)?.toBoolean() ?: false
 
     companion object {
         const val FALSE = "false"
-        const val IMAGE = "img"
+        const val RUNTIME = "runtime"
         const val EXPORT_SESSION = "exportSession"
         const val PULL_IMAGES = "pullImages"
     }
