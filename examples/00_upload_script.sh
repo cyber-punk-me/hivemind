5fd4792070d0c8815b9f02ecc2686d3f7749ceae
kyr7 <kirillskiy@gmail.com>
2018-12-26 21:27:51 +0300

    new nn

--- examples/00_upload_script.sh
+++ examples/00_upload_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #https://app.swaggerhub.com/apis/kyr7/hivemind/0.0.1#/script/scriptPost
 rm master.zip
-wget https://github.com/cyber-punk-me/myo-armband-nn/archive/master.zip
+wget https://github.com/cyber-punk-me/emg-nn/archive/master.zip
 curl -X POST "http://localhost:8080/script/7de76908-d4d9-4ce9-98de-118a4fb3b8f8" \
  -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "upfile=@master.zip;type=application/zip"
