31438c737e9c644a845bd913f02b9fabaab6ae31
IgorPerikov <perikov.igor@gmail.com>
2019-02-26 22:49:43 +0300

    support multiple training image profiles

--- src/main/kotlin/io/cyber/hivemind/model/RunConfig.kt
+++ src/main/kotlin/io/cyber/hivemind/model/RunConfig.kt
@@ -1,12 +1,17 @@
 package io.cyber.hivemind.model
 
-data class RunConfig(val cmd : List<String>, val image: String, val opt : Map<String, String>? = null) {
+data class RunConfig(
+    val cmd: List<String>,
+    val image: String,
+    val opt: Map<String, String>? = null,
+    val devices: List<String>? = null
+) {
 
     fun isExportSession() = getBoolOption(EXPORT_SESSION)
     fun isPullImages() = getBoolOption(PULL_IMAGES)
     fun getRuntime() = opt?.get(RUNTIME)
 
-    private fun getBoolOption(key : String) : Boolean = opt?.getOrDefault(key, FALSE)?.toBoolean() ?: false
+    private fun getBoolOption(key: String): Boolean = opt?.getOrDefault(key, FALSE)?.toBoolean() ?: false
 
     companion object {
         const val FALSE = "false"
