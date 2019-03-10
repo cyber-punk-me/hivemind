8498aec88f43e898772de74d8c91e6363ca52763
kyr7 <kirillskiy@gmail.com>
2019-03-10 17:23:35 +0300

    moved example data out to an external repo

--- examples/01_upload_training_data.sh
+++ examples/01_upload_training_data.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 #https://app.swaggerhub.com/apis/kyr7/hivemind/0.0.1#/data/dataPost
 #generate a uuid and post. each post will create a new file named 0,1,..N
-#you can use data from myo-armband-nn for testing
-data="1.0, 2.0, 5.0"
-curl -d "$data" -X POST http://localhost:8080/data/5d335160-bd2a-45e4-9199-8105a38941ad?ext=csv
+#curl -d "data" -X POST http://localhost:8080/data/5d335160-bd2a-45e4-9199-8105a38941ad?ext=csv
+rm -f data.zip
+rm -r data
+
+wget -O data.zip https://github.com/cyber-punk-me/emg-nn/archive/master.zip
+unzip data.zip -d data
+
+for filename in data/emg-nn-master/data/*.csv; do
+    echo "uploading $filename"
+    #example of posting data the way an app could do it
+    curl --data-binary @- -X POST http://localhost:8080/data/5d335160-bd2a-45e4-9199-8105a38941ad?ext=csv \
+      -H "accept: application/json" <<< "$(cat $filename)"
+done
