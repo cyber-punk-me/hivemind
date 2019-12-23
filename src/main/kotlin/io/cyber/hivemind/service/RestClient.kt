aa0bae2b9b01f7e0ce687c37d1e845e03dfb3ddf
kyr7 <kyashuk@tab-trader.com>
2019-12-23 15:47:36 +0300

    ktor http client setup for tf servable

--- src/main/kotlin/io/cyber/hivemind/service/RestClient.kt
+++ src/main/kotlin/io/cyber/hivemind/service/RestClient.kt
@@ -2,26 +2,24 @@ package io.cyber.hivemind.service
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ObjectNode
-import io.cyber.hivemind.util.fromJson
 import io.cyber.hivemind.util.toJson
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
-//import io.ktor.client.features.json.JsonFeature todo import error
+import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.request.*
 import io.ktor.http.ContentType
 import io.ktor.http.content.TextContent
 
 class RestClient(val host: String, val port: Int) {
 
-    suspend fun applyData(path: String, data: JsonNode): JsonNode {
-        val mlResponse = httpClient.post<String>(scheme = "http", host = host, port = port, path = path,
-                body = TextContent(toJson(data), contentType = ContentType.Application.Json))
-        return fromJson(mlResponse, ObjectNode::class.java)
-    }
+    suspend fun applyData(path: String, data: JsonNode): JsonNode =
+            httpClient.post<ObjectNode>(scheme = "http", host = host, port = port, path = path,
+                    body = TextContent(toJson(data), contentType = ContentType.Application.Json))
 
     companion object {
         private val httpClient = HttpClient(Apache) {
-            //install(JsonFeature) todo import error
+            install(JsonFeature)
         }
     }
+
 }
